3d97b94f503af62aae7ca9eb2e5f02721b1707e5
Ivan <ivan@yodel.to>
2015-12-30 11:25:30 -0800

    Refactored how static files are served.

--- app.py
+++ app.py
@@ -4,16 +4,11 @@ from logging.handlers import RotatingFileHandler
 from flask import Flask
 from flask import send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 @app.route("/")
-def hello():
-    return "Hello World!"
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+def index():
+    return app.send_static_file('index.html')
 
 if __name__ == "__main__":
     handler = RotatingFileHandler('SeattleCrime.log', maxBytes=10000, backupCount=1)
